068391e68d65ac5f84328c84e0255a28e462ec59
eggsy84 <eggsy@eggsylife.co.uk>
2014-04-18 12:03:56 +0100

    Revert exploratory test failure change

--- src/test/java/uk/co/eggsylife/testing/integration/CalculatorControllerIntegrationTest.java
+++ src/test/java/uk/co/eggsylife/testing/integration/CalculatorControllerIntegrationTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -45,8 +44,6 @@ public class CalculatorControllerIntegrationTest {
 					get("/calculator"))
 					.andExpect(status().isOk())
 					.andExpect(model().attributeExists("calculatorCommand"));
-		
-		Assert.assertFalse(true);
 		
 	}
 	
